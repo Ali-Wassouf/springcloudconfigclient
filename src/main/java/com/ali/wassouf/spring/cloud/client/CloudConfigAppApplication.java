7f6040e67a021a37590f0ce54d3e54b966468003
aliwassouf93 <wassouf.ali.eng@gmail.com>
2019-05-08 20:56:50 +0200

    Initital commit

---
+++ src/main/java/com/ali/wassouf/spring/cloud/client/CloudConfigAppApplication.java
@@ -0,0 +1,13 @@
+package com.ali.wassouf.spring.cloud.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudConfigAppApplication.class, args);
+    }
+
+}
